de4424ddb167ada706eda723e236dc7b8c2d59fe
Ciro Xavier <ciroprates@gmail.com>
2025-04-21 22:25:59 -0300

    feat: add ec2:DescribeImages permission to CodeBuild role

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -110,6 +110,21 @@ resource "aws_iam_role_policy_attachment" "codebuild_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess"
 }
 
+resource "aws_iam_role_policy" "codebuild_ec2" {
+  name = "codebuild-ec2-policy"
+  role = aws_iam_role.codebuild_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = [
+        "ec2:DescribeImages"
+      ]
+      Effect   = "Allow"
+      Resource = "*"
+    }]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
